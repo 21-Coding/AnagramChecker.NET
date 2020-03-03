4ee83102d42dd3c4a61f61af718b6714d77095db
Student <example@example.com>
2020-03-03 13:55:51 -0800

    add ui

--- Anagram.Tests/ModelTests/AnagramTests.cs
+++ Anagram.Tests/ModelTests/AnagramTests.cs
@@ -10,20 +10,6 @@ namespace Anagram.Tests
   // public class WordsTests : IDisposable
   public class WordsTests
   {
-    // public void Dispose()
-    // {
-    //   // clear
-    // }
-    // [TestMethod]
-    // public void WordMatch_ReturnsTrueIfExists_True()
-    // {
-    //   //arrange
-    //   string inputWord = "apple";
-    //   bool result = Words.WordMatch(inputWord);
-
-    //   Assert.AreEqual(true, result);
-    // }
-
     [TestMethod]
     public void CheckWord_MatchDictionaryWords_True()
     {
--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using Anagram.Models;
 
 namespace Anagram
 {
-    class Program
+  class Program
+  {
+    public static void QuickText(string line)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      for (int i = 0; i < line.Length; i++)
+      {
+        Console.Write(line[i]);
+        System.Threading.Thread.Sleep(50);
+      }
     }
+    static void Main(string[] args)
+    {
+      QuickText(@"Is your word an Anagram?
+      Enter : ");
+
+      string input = Console.ReadLine();
+
+      List<string> wordList = Words.WordMatch(input);
+      int i = 0;
+      foreach (var item in wordList)
+      {
+        ++i;
+        QuickText($"{i}. {item} \n");
+      }
+    }
+  }
 }
